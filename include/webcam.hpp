41150172edff75bab74c7e7d4316ad5da33236ad
Chris Dallat <chr15dallat@hotmail.co.uk>
2022-10-23 19:17:51 +0100

    breaking down into classes

--- include/webcam.hpp
+++ include/webcam.hpp
@@ -11,11 +11,25 @@ class Webcam
         Webcam();
         ~Webcam();
 
-
+        void                capture();
+        // void                loop();
+        void                wait() const;
+        bool                valid() const;
 
     private:
-        int                m_camera;
-        cv::Mat            m_mat;
+        int                 m_camera = 0;
+        bool                m_valid = true;
+        cv::VideoCapture    m_capture;
+        cv::Mat             m_mat_original; //Matrix Object, input from webcam
+        cv::Mat             m_mat_processed;//Matrix Object, processed image 
+        
+        std::vector<cv::Vec3f> m_vecCircles; //3 element vector of floats, pass by ref output to HoughCircles()
+        std::vector<cv::Vec3f>::iterator m_itrCircles; //iterator for vecCircles vector
+        
+        int                 m_video_width = 640;
+        int                 m_video_height = 480;
+
+
 };
 
 #endif //WEBCAM_HPP
--- src/webcam.cpp
+++ src/webcam.cpp
@@ -4,11 +4,39 @@
 
 Webcam::Webcam() 
 {
-    std::cout << "Constructed Webcam" << std::endl;
+    if (m_capture.open(m_camera) == false) 
+    {
+    std::cout << "Failed to open camera" << std::endl;
+    m_valid = false;
+    }
+
+    m_capture.set(cv::CAP_PROP_FRAME_WIDTH, m_video_width);
+    m_capture.set(cv::CAP_PROP_FRAME_HEIGHT, m_video_height);
+    m_mat_original = cv::Mat::zeros(m_video_height, m_video_width, CV_8UC3);
 }
 
 Webcam::~Webcam() 
 {
-    std::cout << "Destroyed Webcam" << std::endl;
+  m_capture.release();
+  cv::destroyAllWindows();
 }
+
+void Webcam::capture() 
+{
+    while(m_valid)
+    {
+        if(m_capture.read(m_mat_original) == false)
+        {
+            std::cout << "error: frame not read from webcam" << std::endl;
+            m_valid = false;
+        }
+        cv::flip(matOriginal, matOriginal, 1); //1 flips around y-axis
+    }
+}
+
+void Webcam::wait() const { cv::waitKey(25); }
+
+bool Webcam::valid() const { return m_valid; }
+
+// void Webcam::loop() {
 
